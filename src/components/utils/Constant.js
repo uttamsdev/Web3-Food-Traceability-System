6e39f6bcde7c1730f2f418aa95ee4e877b259d75
uttamsdev <uttamsdev@gmail.com>
2024-09-27 18:03:28 +0600

    credentials update

--- src/components/utils/Constant.js
+++ src/components/utils/Constant.js
@@ -1,3 +1,3 @@
 import abi from "./abi.json";
 export const contractABI = abi; //for hardhat abi.abi
-export const contractAddress = "0xBE17787a2E408736a557a5A83f73A5f9097BE3D5";
+export const contractAddress = "0x6aC9a9a4106E17bd6A1A593F17327eBB7E2ad9bf";
